3d1a0d8c28e15ccb609fc64f8774cfbc93ca78e2
chainstrument <minitutoriel@gmail.com>
2022-08-25 14:27:50 +0200

    add form front

--- controllers/front/display.php
+++ controllers/front/display.php
@@ -5,6 +5,8 @@ if(!defined('_PS_VERSION_'))
     exit;
 }
 
+include_once __DIR__ . '/../../classes/Messages.php';
+
 class cl_messagesclientdisplayModuleFrontController extends ModuleFrontController
 {
 
@@ -18,11 +20,40 @@ class cl_messagesclientdisplayModuleFrontController extends ModuleFrontControlle
     public function initContent()
     {
         parent::initContent();
+
+
+        $options = array();
+        foreach (Customer::getCustomers() as $user)
+        {
+          $options[] = array(
+            "id_customer" => (int)$user['id_customer'],
+            "name" => $user['firstname']
+          );
+        }
+
+        // In the template, we need the vars paymentId & paymentStatus to be defined
+  $this->context->smarty->assign(
+    array(
+      'users' => $options, // Retrieved from GET vars
+    
+    ));
   
         $this->setTemplate('module:cl_messagesclient/views/templates/front/display.tpl');
 
 
     }
 
+
+    public function postProcess()
+    {
+        parent::postProcess();
+
+        $message = new Messages();
+        $message->subject = Tools::getValue('subject');
+        $message->message = Tools::getValue('message');
+        $message->id_user = Tools::getValue('id_customer');
+        dump($message);
+        $message->save();
+    }
 
 }
--- views/templates/front/display.tpl
+++ views/templates/front/display.tpl
@@ -1,6 +1,38 @@
 {extends file='customer/page.tpl'}
 
+
+{block name="page_title"}
+  {l s='New message' d='Modules.cl_messagesclient.Shop'}
+{/block}
+
 {block name="page_content"}
-    Me voici 
+    
+<section class="login-form">
+ 
+  <form action="" method="post"  >
+
+    <label>
+        <span>{l s='List of users' d='Modules.cl_messagesclient.Shop'}</span>
+        
+        <select name="id_customer">
+        {foreach from=$users item=user}
+            <option value="{$user.id_customer|escape:'htmlall':'UTF-8'}">{$user.name}</option>
+        {/foreach}
+        </select>
+    </label>
+       <label>
+          <span>{l s='Subject' d='Modules.cl_messagesclient.Shop'}</span>
+          <input type="text" name="subject" value="" />
+        </label>
+
+  <label>
+        <span>{l s='Message' d='Modules.cl_messagesclient.Shop'}</span>
+        <textarea cols="67" rows="3" name="message"> </textarea>
+    </label>
+ <button type="submit" name="submitMessage">
+          {l s='Send' d='Modules.cl_messagesclient.Shop'}
+        </button>
+        </form>
+</section>
 
 {/block}
